6ae2f60df8916dc9126afc687f977cfd91ebc66e
unknown <fobus1289>
2021-08-12 17:04:37 +0500

    delete not need file

--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/fobus1289/marshrudka
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.7.2
 	github.com/gorilla/websocket v1.4.2
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/rs/cors v1.8.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- test/main.go
+++ test/main.go
@@ -1,114 +1,142 @@
+//example 1
 package main
 
 import (
 	"github.com/fobus1289/marshrudka/router"
-	"github.com/rs/cors"
 	"log"
-	"net/http"
+	"math/rand"
 )
 
-func CORSEnabledFunction(w http.ResponseWriter, r *http.Request) interface{} {
-	// Set CORS headers for the preflight request
-	///*
-	//	//ctx:= r.WithContext(context.Background())
-	//	hj, ok := w.(http.Hijacker)
-	//
-	//	if ok {
-	//
-	//		conn, _, _ := hj.Hijack()
-	//
-	//		conn.Close()
-	//
-	//		return router.Throw{
-	//			StatusCode: 204,
-	//		}
-	//	}
-	//*/
-	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Max-Age", "3600")
-		w.WriteHeader(http.StatusNoContent)
-		return router.Throw{
-			StatusCode: 204,
+type User struct {
+	Id    int64  `json:"id"`
+	Login string `json:"login"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+}
+
+type Users []*User
+
+var ResponseUsers = Users{
+	&User{
+		Id:    1,
+		Login: "login_1",
+		Name:  "Jhone",
+		Age:   18,
+	},
+	&User{
+		Id:    2,
+		Login: "login_2",
+		Name:  "Doe",
+		Age:   35,
+	},
+	&User{
+		Id:    3,
+		Login: "login_3",
+		Name:  "Bob",
+		Age:   27,
+	},
+}
+
+/*
+	Параметры могут быть пустыми
+	Возвращаемый тип может быть любым
+	interface{} итд, если это ссылочный тип, то он преобразуется в json
+*/
+func GetAllUsers() Users {
+	log.Println("get all users")
+	return ResponseUsers
+}
+
+/*
+	если не известный параметр в функции и он ссылочный типа то он конвертироваеца в тип в параметре
+	преметивы не допускаются
+*/
+func CreateUser(user *User) interface{} {
+
+	for _, responseUser := range ResponseUsers {
+		if responseUser.Login == user.Login {
+			return router.Response(400).
+				Throw().Json(
+				map[string]string{
+					"message": "user login is duplicate",
+				},
+			)
 		}
 	}
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	return nil
+	user.Id = rand.Int63() % 100
+
+	return user
+}
+
+/*
+*router.Request это служебная структура роутера
+ */
+func DeleteUser(request *router.Request) interface{} {
+	//var id = request.Query("id")
+	//var id = request.QueryGetInt("id")
+
+	var id int64
+
+	if !request.TryQueryGetInt("id", &id) {
+		return router.Response(400).
+			Throw().Json(
+			map[string]string{
+				"message": "id can be empty or string",
+			},
+		)
+	}
+
+	for i, user := range ResponseUsers {
+		if user.Id == id {
+
+			ResponseUsers = append(ResponseUsers[:i], ResponseUsers[i+1:]...)
+
+			return true
+			//return 1
+			//return "user deleted" + strconv.FormatInt(id, 10)
+			//return user
+		}
+	}
+
+	return router.Response(400).
+		Throw().Json(
+		map[string]string{
+			"message": "user not found",
+		},
+	)
+}
+
+/*
+	Динамическая реализация параметров таких как
+	http.ResponseWriter
+	*http.Request
+ 	*router.Request
+*/
+func GetUserById(request *router.Request) interface{} {
+	var id = request.QueryGetInt("id")
+
+	for _, user := range ResponseUsers {
+		if user.Id == id {
+			return router.Response(200).Json(user)
+		}
+	}
+
+	return router.Response(404).Throw().Json(
+		map[string]string{
+			"message": "user not found",
+		},
+	)
+
 }
 
 func main() {
+	var drive = router.NewRouter()
 
-	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{"*"},
-		AllowedHeaders: []string{"*"},
-	})
-	//c := cors.New(cors.Options{
-	//	AllowedOrigins: []string{"http://foo.com", "http://foo.com:8080"},
-	//	AllowCredentials: true,
-	//	// Enable Debugging for testing, consider disabling in production
-	//	Debug: true,
-	//})
-	route := router.NewRouter()
-	_ = c
+	drive.GET("/", GetAllUsers)
+	drive.POST("/", CreateUser)
+	drive.DELETE("/", DeleteUser)
+	drive.GET("/by-id", GetUserById)
 
-	route.Use(CORSEnabledFunction)
-
-	//route.Use(func(w http.ResponseWriter, r *http.Request) {
-	//	log.Println(r.Method)
-	//	w.Header().Set("Accept", "*/*")
-	//	w.Header().Set("Cache-Control", "no-cache")
-	//	w.Header().Set("Accept-Encoding", "gzip, deflate, br")
-	//	w.Header().Set("Access-Control-Allow-Origin", "*")
-	//	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	//	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	//	w.Header().Set("Access-Control-Max-Age", "3600")
-	//	//w.WriteHeader(http.StatusNoContent)
-	//})
-
-	//route.Use(CORSEnabledFunction)
-
-	route.MATCH("/", []string{"get", "post", "put"}, func() {
-
-	})
-
-	adminGroup := route.Group("/admin")
-	{
-		adminGroup.GET("/user", func() {
-			println("admin/user")
-		})
-
-		adminCompanyGroup := adminGroup.Group("/company")
-		{
-			adminCompanyGroup.GET("/user/", func() {
-				println("admin/company/user get")
-			})
-			adminCompanyGroup.POST("/user/", func() {
-				println("admin/company/user post")
-			})
-		}
-
-	}
-
-	route.ANY("/any/:id{n}", func(request *router.Request) interface{} {
-
-		//log.Println(request.Request.ParseMultipartForm(0))
-		//log.Println(request.Request.MultipartForm)
-
-		file, err := request.FormFile("file")
-
-		if err != nil {
-			return router.Response(400).Throw().Text("bad request")
-		}
-
-		err = file.SetName("rd").Store("static")
-
-		log.Println(err)
-		return router.Response(200).File("static/test.txt", "ok.txt").Download()
-	})
-
-	route.Run("127.0.0.1:8080")
+	drive.Run(":8081")
 }
